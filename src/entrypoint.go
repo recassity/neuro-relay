16a9a3a6c8ed60b8c45dcc4966f28d692a030991
Cassity <164010909+recassity@users.noreply.github.com>
2026-01-29 12:45:23 +0700

    Update entrypoint.go

--- src/entrypoint.go
+++ src/entrypoint.go
@@ -1,32 +1,59 @@
-package entrypoint
-// this is the entrypoint for all the other files for neuro-relay
-
 package main
 
 import (
-    "log"
-    "github.com/cassitly/neuro-integration-sdk"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/recassity/neuro-relay/src/nintegration"
 )
 
 func main() {
-    // Create client
-    client, err := neuro.NewClient(neuro.ClientConfig{
-        Game:         "Game Hub",
-        WebsocketURL: "ws://localhost:8000",
-    })
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Parse command line flags
+	relayName := flag.String("name", "Game Hub", "Name of the relay shown to Neuro")
+	neuroURL := flag.String("neuro-url", "ws://localhost:8000", "Neuro backend WebSocket URL")
+	emulatedAddr := flag.String("emulated-addr", "127.0.0.1:8001", "Address for emulated backend")
+	flag.Parse()
 
-    // Connect
-    if err := client.Connect(); err != nil {
-        log.Fatal(err)
-    }
-    defer client.Close()
+	log.Println("=================================")
+	log.Println("  NeuroRelay - Integration Hub   ")
+	log.Println("=================================")
+	log.Printf("Version: %s", "1.0.0")
+	log.Println()
 
-    // Send context
-    client.SendContext("Game started!", false)
+	// Create integration client
+	client, err := nintegration.NewIntegrationClient(nintegration.IntegrationClientConfig{
+		RelayName:    *relayName,
+		NeuroURL:     *neuroURL,
+		EmulatedAddr: *emulatedAddr,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create integration client: %v", err)
+	}
 
-    // Register actions (see examples below)
-    // ...
+	// Start the relay system
+	if err := client.Start(); err != nil {
+		log.Fatalf("Failed to start relay: %v", err)
+	}
+
+	log.Println()
+	log.Println("NeuroRelay is running!")
+	log.Println("- Games can connect to: ws://" + *emulatedAddr + "/ws")
+	log.Println("- Connected to Neuro as: " + *relayName)
+	log.Println()
+	log.Println("Waiting for game integrations to connect...")
+	log.Println("Press Ctrl+C to stop")
+	log.Println()
+
+	// Wait for interrupt signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+
+	log.Println()
+	log.Println("Shutting down NeuroRelay...")
+	client.Stop()
+	log.Println("Goodbye!")
 }
