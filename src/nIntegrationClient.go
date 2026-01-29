b1fdc418a827fbb6198d4c98ac6daa3619f9230e
Cassity <164010909+recassity@users.noreply.github.com>
2026-01-29 11:29:56 +0700

    Create nIntegrationClient.go

---
+++ src/nIntegrationClient.go
@@ -0,0 +1,3 @@
+package nIntegration
+// this file is what defines the code for neuro-relay to connect to the real neuro backend,
+// and relay the processed commands from integrations connected to the emulated backend.
